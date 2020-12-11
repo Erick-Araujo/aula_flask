8e6f92843816c9f93a4f2fbf137418763200511a
feulo-ocean <guilherme.feulo@oceanbrasil.com>
2020-12-11 19:47:42 +0000

    templates e secret key

--- flaskr.py
+++ flaskr.py
@@ -7,10 +7,10 @@ from flask import Flask, render_template, request, g, redirect, url_for, \
 DATABASE = './tmp/flaskr.db'
 USERNAME = 'admin'
 PASSWORD = '123456'
-
+SECRET_KEY= 'teste'
 # Criando o App
 app = Flask(__name__)
-
+app.config.from_object(__name__)
 def conectar_bd():
     return sqlite3.connect(DATABASE)
 
@@ -27,7 +27,7 @@ def exibir_entradas():
     sql = "SELECT titulo, texto FROM entradas ORDER BY id DESC"
     cursor = g.bd.execute(sql)
     entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cursor.fetchall()]
-    return render_template('exibir_entradas.html')
+    return render_template('exibir_entradas.html', entradas=entradas)
 
 @app.route('/inserir', methods=['POST'])
 def inserir_entrada():
