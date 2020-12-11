3e3da722badb00628f1799815597ed5d269ad218
feulo-ocean <guilherme.feulo@oceanbrasil.com>
2020-12-11 19:22:29 +0000

    todas as rotas prontas

--- flaskr.py
+++ flaskr.py
@@ -28,3 +28,36 @@ def exibir_entradas():
     cursor = g.bd.execute(sql)
     entradas = [dict(titulo=titulo, texto=texto) for titulo, texto in cursor.fetchall()]
     return render_template('exibir_entradas.html')
+
+@app.route('/inserir', methods=['POST'])
+def inserir_entrada():
+    if not session.get('logado'):
+        abort(401)
+    sql = "INSERT INTO entradas (titulo, texto) VALUES (?, ?)"
+    g.bd.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.bd.commit()
+    flash('Nova entrada salva com sucesso!')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        if request.form['username'] != USERNAME:
+            erro = "Usuário Inválido"
+        elif request.form['password'] != PASSWORD:
+            erro = 'Senha inválida'
+        else:
+            session['logado'] = True
+            flash('Logado com Sucesso. Bem-vindo!')
+            return redirect(url_for('exibir_entradas'))
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    flash('O logout foi feito com sucesso')
+    return redirect(url_for('exibir_entradas'))
+  
+
+        
